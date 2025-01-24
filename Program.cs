5af57e86c9c040e0f9e302895a398c42954a6012
StephanHoeksema <s.hoeksema@windehsheim.nl>
2025-01-24 18:18:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ShaConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            string[] names = { "Stephan Hoeksema", "Piet" };
+            /*Console.WriteLine("Hello, welkom " + name); //Concationation
+             * Console.WriteLine($"Hello, welkom {name}" ); //Intrapolation
+             */ 
+            Console.WriteLine("Hello, welkom {1}",names); //Formatted string
+             /* 
+             * int myAge = 48; 
+             * Console.WriteLine(myAge);
+             * */
+
+            Console.WriteLine("Ik wil graag wat informatie van je vastleggen.");
+            Console.WriteLine("Your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Year of Birth");
+            int YoB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your name {0}.", name);
+            Console.WriteLine("Your age is: " + (2024 - YoB));
+            Console.WriteLine("Your retirement starts in " + (YoB + 68 - 2024) + " years.");
+            
+        }
+    }
+}
