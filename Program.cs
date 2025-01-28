dbc00fc1a7050fc3ede7ecb60e75a1990d566f13
StephanHoeksema <s.hoeksema@windehsheim.nl>
2025-01-28 16:13:56 +0100

    Start debug

--- Program.cs
+++ Program.cs
@@ -10,14 +10,26 @@
              * Console.WriteLine($"Hello, welkom {name}" ); //Intrapolation
              */ 
             Console.WriteLine("Hello, welkom {0}",names); //Formatted string
-             /* 
-             * int myAge = 48; 
-             * Console.WriteLine(myAge);
-             * */
+            /* 
+            * int myAge = 48; 
+            * Console.WriteLine(myAge);
+            * */
 
+            /*
+             * Variables
+             */
+
+            string name = string.Empty; //Define so it doesn't have a null value
+            int YoB = 0;
+            decimal salary;
+            char gender;
+
+            /*
+             * Output
+             */
             Console.WriteLine("Ik wil graag wat informatie van je vastleggen.");
             Console.WriteLine("Your name: ");
-            string name = Console.ReadLine();
+            name = Console.ReadLine();
             Console.WriteLine("Year of Birth");
             int YoB = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your name {0}.", name);
